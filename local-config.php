749d0a6c1a7f9300992f666f7773e6027d1e21d0
julius hagen <julius.hagen@imwebsein.de>
2015-06-18 14:13:52 +0200

    config anpassung

--- local-config.php
+++ local-config.php
@@ -6,7 +6,7 @@ In it, you *must* include the four main database defines
 You may include other settings here that you only want enabled on your local development checkouts
 */
 
-define( 'DB_NAME', 'd01e8d8d' );
-define( 'DB_USER', 'd01e8d8d' );
-define( 'DB_PASSWORD', '6vKk2fFNS2BGa9LS' );
-define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
+define( 'DB_NAME', 'gopagoda' );
+define( 'DB_USER', 'pasty' );
+define( 'DB_PASSWORD', 'rnMP15Yk' );
+define( 'DB_HOST', '192.168.0.3' ); // Probably 'localhost'
